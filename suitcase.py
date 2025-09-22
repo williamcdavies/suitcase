c5cc468003f5f1cde9d0226bb1f2c090b973423c
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-09-22 15:41:48 -0700

    Add anchor to stripped href logic

--- suitcase.py
+++ suitcase.py
@@ -3,15 +3,20 @@ import asyncio
 from playwright.async_api import async_playwright
 
 async def main() -> int:
-        str = input("stdin: ")
+        url = input("goto?: ")
         
         async with async_playwright() as p:
-                browser = await p.firefox.launch(headless=False)
+                browser = await p.chromium.launch(headless=False)
                 page = await browser.new_page()
-                await page.goto(str)
-                print(await page.title())
-                await browser.close()
+                await page.goto(url)
 
+                anchors = page.locator("a")
+                hrefs = await anchors.evaluate_all(
+                        "anchors => anchors.filter(anchor => anchor.hasAttribute('href')).map(anchor => anchor.href).filter(h => h.match(/.*\\.[^\\/]+$/i))"
+                        )
+                print(hrefs)
+                
+                await browser.close()
         return 0
 
 if __name__ == '__main__':

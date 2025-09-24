f26ff659729ed240a9e1c97a94d8b9980192c8cf
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-09-24 07:57:54 -0700

    Rename p -> path for clarity

--- suitcase/cfg.py
+++ suitcase/cfg.py
@@ -3,9 +3,9 @@ import json
 from pathlib import Path
 
 def init():
-        p = Path("config.json")
-        if not p.exists():
-                p.write_text(json.dump("{\"filters\": [\".kml\", \".txt\", \".zip\"]}"))
+        path = Path("config.json")
+        if not path.exists():
+                path.write_text(json.dump("{\"filters\": [\".kml\", \".txt\", \".zip\"]}"))
 
 def load():
         with open("config.json", "r") as rf: 

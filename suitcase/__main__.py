9ad1aa11fb9dbe87deed237d80591e445a962025
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-09-29 13:44:41 -0700

    Refactor: split urls into expression, arg, urls
    Added pathlib import for 27. Removed type suggestion from import string

--- suitcase/__main__.py
+++ suitcase/__main__.py
@@ -1,12 +1,13 @@
 import asyncio
 
+from pathlib                    import Path
 from playwright.async_api       import async_playwright
 from suitcase                   import config
 from suitcase                   import scripts
 from suitcase                   import session
 
 async def main() -> int:
-        url = input("__main__.py, url, [str]% ")
+        url = input("__main__.py, url% ")
 
         async with async_playwright() as p:
                 browser = await p.chromium.launch(headless=False)
@@ -14,14 +15,16 @@ async def main() -> int:
                 
                 await page.goto(url)
 
-                handover = 1 if input("__main__.py, handover, [y/N]% ") == "y" else 0
+                handover = 1 if input("__main__.py, handover% ") == "y" else 0
                 if(not handover):
                         await browser.close()
                         return 0
                 
-                anchors = page.locator("a")
-                urls    = await anchors.evaluate_all(scripts.read("injection.js"), config.load()["filters"])
-                path    = input("__main__.py, path, [Path]% ")
+                anchors         = page.locator("a")
+                expression      = scripts.read("injection.js")
+                arg             = config.load()["filters"]
+                urls            = await anchors.evaluate_all(expression, arg)
+                path            = Path(input("__main__.py, path% "))
                 
                 await session.download(urls, path)
                 await browser.close()

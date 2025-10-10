cff01fdb06cc1463c3924f326579571ed195b183
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-10-10 14:13:24 -0700

    Modify whitespace

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='suitcase',
-    version='1.0',
-    packages=find_packages(),
-)
+        name='suitcase',
+        version='1.0',
+        packages=find_packages(),
+    )
